ac466fd01da0c8cc7855260655402f4428132c02
Daniel Kiper <daniel.kiper@oracle.com>
2015-01-30 18:54:21 +0100

    x86/efi: create new early memory allocator
    There is a problem with place_string() which is used as early memory
    allocator. It gets memory chunks starting from start symbol and
    going down. Sadly this does not work when Xen is loaded using multiboot2
    protocol because start lives on 1 MiB address. So, I tried to use
    mem_lower address calculated by GRUB2. However, it works only on some
    machines. There are machines in the wild (e.g. Dell PowerEdge R820)
    which uses first ~640 KiB for boot services code or data... :-(((
    
    In case of multiboot2 protocol we need that place_string() only allocate
    memory chunk for EFI memory map. However, I think that it should be fixed
    instead of making another function used just in one case. I thought about
    two solutions.
    
    1) We could use native EFI allocation functions (e.g. AllocatePool()
       or AllocatePages()) to get memory chunk. However, later (somewhere
       in __start_xen()) we must copy its contents to safe place or reserve
       this in e820 memory map and map it in Xen virtual address space.
       In later case we must also care about conflicts with e.g. crash
       kernel regions which could be quite difficult.
    
    2) We may allocate memory area statically somewhere in Xen code which
       could be used as memory pool for early dynamic allocations. Looks
       quite simple. Additionally, it would not depend on EFI at all and
       could be used on legacy BIOS platforms if we need it. However, we
       must carefully choose size of this pool. We do not want increase
       Xen binary size too much and waste too much memory but also we must fit
       at least memory map on x86 EFI platforms. As I saw on small machine,
       e.g. IBM System x3550 M2 with 8 GiB RAM, memory map my contain more
       than 200 entries. Every entry on x86-64 platform is 40 bytes in size.
       So, it means that we need more than 8 KiB for EFI memory map only.
       Additionally, if we want to use this memory pool for Xen and modules
       command line storage (it would be used when xen.efi is executed as EFI
       application) then we should add, I think, about 1 KiB. In this case,
       to be on safe side, we should assume at least 64 KiB pool for early
       memory allocations, which is about 4 times of our earlier calculations.
       If we think that we should not waste unallocated memory in the pool
       on running system then we can mark this region as __initdata and move
       all required data to dynamically allocated places somewhere in __start_xen().
    
    Now solution #2 is implemented but maybe we should consider #1 too.
    
    Signed-off-by: Daniel Kiper <daniel.kiper@oracle.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -1123,8 +1123,7 @@ void __init noreturn __start_xen(unsigned long mbi_p)
         panic("Not enough memory to relocate Xen.");
 
     /* This needs to remain in sync with xen_in_range(). */
-    reserve_e820_ram(&boot_e820, efi_loader ? mbi->mem_upper : __pa(_stext),
-                     __pa(__2M_rwdata_end));
+    reserve_e820_ram(&boot_e820, __pa(_start), __pa(__2M_rwdata_end));
 
     /* Late kexec reservation (dynamic start address). */
     kexec_reserve_area(&boot_e820);
