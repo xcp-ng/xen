cdcc8d99c2c2fa896629ff9007e2a319c4e722d7
Ross Lagerwall <ross.lagerwall@citrix.com>
2015-05-13 12:44:29 +0100

    x86/HVM: Avoid cache flush operations during hvm_load
    An MTRR record is processed for each vCPU during hvm_load. Each MTRR
    record sets several mtrrs, each of which flushes the cache on all pCPUs.
    This can take some time and trip the watchdog for HVM guests with many
    CPUs.
    
    To fix this, introduce a flag which prevents flushing the cache when
    doing MTRR operations and instead do a flush at the end of hvm_load.
    
    This reduces the time to restore an HVM guest with 32 vCPUs by about 5
    seconds.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- xen/arch/x86/hvm/save.c
+++ xen/arch/x86/hvm/save.c
@@ -291,6 +291,7 @@ int hvm_load(struct domain *d, hvm_domain_context_t *h)
     struct hvm_save_descriptor *desc;
     hvm_load_handler handler;
     struct vcpu *v;
+    int ret = 0;
 
     if ( d->is_dying )
         return -EINVAL;
@@ -307,6 +308,8 @@ int hvm_load(struct domain *d, hvm_domain_context_t *h)
         if ( !test_and_set_bit(_VPF_down, &v->pause_flags) )
             vcpu_sleep_nosync(v);
 
+    this_cpu(memory_type_changed_ignore) = 1;
+
     for ( ; ; )
     {
         if ( h->size - h->cur < sizeof(struct hvm_save_descriptor) )
@@ -315,13 +318,14 @@ int hvm_load(struct domain *d, hvm_domain_context_t *h)
             printk(XENLOG_G_ERR
                    "HVM%d restore: save did not end with a null entry\n",
                    d->domain_id);
-            return -1;
+            ret = -1;
+            goto out;
         }
 
         /* Read the typecode of the next entry  and check for the end-marker */
         desc = (struct hvm_save_descriptor *)(&h->data[h->cur]);
         if ( desc->typecode == 0 )
-            return 0;
+            goto out;
 
         /* Find the handler for this entry */
         if ( (desc->typecode > HVM_SAVE_CODE_MAX) ||
@@ -329,7 +333,8 @@ int hvm_load(struct domain *d, hvm_domain_context_t *h)
         {
             printk(XENLOG_G_ERR "HVM%d restore: unknown entry typecode %u\n",
                    d->domain_id, desc->typecode);
-            return -1;
+            ret = -1;
+            goto out;
         }
 
         /* Load the entry */
@@ -339,11 +344,17 @@ int hvm_load(struct domain *d, hvm_domain_context_t *h)
         {
             printk(XENLOG_G_ERR "HVM%d restore: failed to load entry %u/%u\n",
                    d->domain_id, desc->typecode, desc->instance);
-            return -1;
+            ret = -1;
+            goto out;
         }
     }
 
-    /* Not reached */
+    ASSERT_UNREACHABLE();
+
+out:
+    this_cpu(memory_type_changed_ignore) = 0;
+    memory_type_changed(d);
+    return ret;
 }
 
 int _hvm_init_entry(struct hvm_domain_context *h, uint16_t tc, uint16_t inst,
