0c924f74259eca0cb0eaea272c19e3bf0e659ff3
Ross Lagerwall <ross.lagerwall@citrix.com>
2015-05-27 10:04:00 +0100

    libxl: Don't insert PCI device into xenstore for HVM guests
    When doing passthrough of a PCI device for an HVM guest, don't insert
    the device into xenstore, otherwise pciback attempts to use it which
    conflicts with QEMU.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- tools/libxl/libxl_pci.c
+++ tools/libxl/libxl_pci.c
@@ -1106,7 +1106,7 @@ out:
         }
     }
 
-    if (!starting)
+    if (!starting && type == LIBXL_DOMAIN_TYPE_PV)
         rc = libxl__device_pci_add_xenstore(gc, domid, pcidev, starting);
     else
         rc = 0;
