fbe1a41b64ec6b0b4e1ce50e6123f7a5799c20d7
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Hypercall to obtain aggregated domain runstate information, and bindings.
    The "legacy" struct xen_domctl_runstate_info cannot change.  The Ocaml
    types/functions (runstate, domain_get_runstate_info, etc) cannot change.
    
    The newer xen_domctl_runstate_info_ext can be extended if a new Ocaml
    Runstateinfo.Vn module is created, and become set in stone when the next build
    of Xen is produced.

--- xen/include/xen/sched.h
+++ xen/include/xen/sched.h
@@ -376,6 +376,8 @@ struct evtchn_port_ops;
 
 #define MAX_NR_IOREQ_SERVERS 8
 
+typedef struct xen_domctl_runstate_info_ext domain_runstate_info_t;
+
 struct domain
 {
     domid_t          domain_id;
@@ -617,6 +619,11 @@ struct domain
 
     /* Holding CDF_* constant. Internal flags for domain creation. */
     unsigned int cdf;
+
+    /* Domain runstates */
+    spinlock_t runstate_lock;
+    atomic_t runstate_missed_changes;
+    domain_runstate_info_t runstate;
 } __aligned(PAGE_SIZE);
 
 static inline struct page_list_head *page_to_list(
@@ -1048,6 +1055,7 @@ int vcpu_affinity_domctl(struct domain *d, uint32_t cmd,
 
 struct vcpu_runstate_extra vcpu_runstate_get(
     const struct vcpu *v, struct vcpu_runstate_info *runstate);
+void domain_runstate_get(struct domain *d, domain_runstate_info_t *info);
 
 uint64_t get_cpu_idle_time(unsigned int cpu);
 void sched_guest_idle(void (*idle) (void), unsigned int cpu);
