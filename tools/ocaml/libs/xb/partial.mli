7af7444c42c9fcb218247170a61c9ac72b3c900b
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    oxenstore-large-packets.patch

--- tools/ocaml/libs/xb/partial.mli
+++ tools/ocaml/libs/xb/partial.mli
@@ -8,6 +8,7 @@ type pkt = {
 external header_size : unit -> int = "stub_header_size"
 external header_of_string_internal : string -> int * int * int * int
   = "stub_header_of_string"
+val allow_oversize_packets : bool ref
 val xenstore_payload_max : int
 val of_string : string -> pkt
 val append : pkt -> string -> int -> unit
