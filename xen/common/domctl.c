87d12cb210a0c3f74df28fc0bf35bf16181e5632
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Collect domain runstate information in the hypervisor, and export through a domctl operation.

--- xen/common/domctl.c
+++ xen/common/domctl.c
@@ -1140,6 +1140,11 @@ long do_domctl(XEN_GUEST_HANDLE_PARAM(xen_domctl_t) u_domctl)
                                      op->u.set_gnttab_limits.maptrack_frames);
         break;
 
+    case XEN_DOMCTL_get_runstate_info:
+        domain_runstate_get(d, &op->u.domain_runstate);
+        copyback = 1;
+        break;
+
     default:
         ret = arch_do_domctl(op, d, u_domctl);
         break;
