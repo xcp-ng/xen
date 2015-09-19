0b978f1e74af8ff8dfe9d2cb880be0cbe04e545f
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2015-09-19 06:09:42 +0000

    tools/xc: Add xc_domain_claim_pages_node()
    Add xc_domain_claim_pages_node(domid, node, nr_pages) to make an exact-node
    claim. node=NUMA_NO_NODE means to make a regular any-node claim like the
    existing call xc_domain_claim_pages() does.
    
    No functional change to xc_domain_claim_pages().
    
    Proposed-by: Marcus Granado <marcus.granado@citrix.com>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- xen/common/memory.c
+++ xen/common/memory.c
@@ -1783,6 +1783,9 @@ long do_memory_op(unsigned long cmd, XEN_GUEST_HANDLE_PARAM(void) arg)
         break;
 
     case XENMEM_claim_pages:
+    {
+        nodeid_t node = NUMA_NO_NODE;
+
         if ( llc_coloring_enabled )
             return -EOPNOTSUPP;
 
@@ -1798,8 +1801,17 @@ long do_memory_op(unsigned long cmd, XEN_GUEST_HANDLE_PARAM(void) arg)
         if ( reservation.extent_order != 0 )
             return -EINVAL;
 
-        if ( reservation.mem_flags != 0 )
+        if ( reservation.mem_flags &
+             ~(XENMEMF_node(0xFE) | XENMEMF_exact_node_request) )
             return -EINVAL;
+
+        if ( reservation.mem_flags & XENMEMF_exact_node_request )
+        {
+            /* domain_set_outstanding_pages() checks the validity of the node */
+            node = XENMEMF_get_node(reservation.mem_flags);
+            if ( node == NUMA_NO_NODE )
+                return -EINVAL;
+        }
 
         d = rcu_lock_domain_by_id(reservation.domid);
         if ( d == NULL )
@@ -1811,12 +1823,13 @@ long do_memory_op(unsigned long cmd, XEN_GUEST_HANDLE_PARAM(void) arg)
             rc = -EINVAL;
 
         if ( !rc )
-            rc = domain_set_outstanding_pages(d, NUMA_NO_NODE,
+            rc = domain_set_outstanding_pages(d, node,
                                               reservation.nr_extents);
 
         rcu_unlock_domain(d);
 
         break;
+    }
 
     case XENMEM_get_vnumainfo:
     {
