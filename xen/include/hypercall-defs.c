6d3b7b80d32d3fa336b47b4ca17226c05b4d2e48
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 07:09:42 +0100

    add-pv-iommu-headers

--- xen/include/hypercall-defs.c
+++ xen/include/hypercall-defs.c
@@ -213,6 +213,7 @@ hypfs_op(unsigned int cmd, const char *arg1, unsigned long arg2, void *arg3, uns
 #ifdef CONFIG_X86
 xenpmu_op(unsigned int op, xen_pmu_params_t *arg)
 #endif
+iommu_op(void *arg, unsigned int count)
 
 #ifdef CONFIG_PV
 caller: pv64
@@ -301,5 +302,6 @@ mca                                do       do       -        -        -
 #if defined(CONFIG_X86) && defined(CONFIG_PAGING) && !defined(CONFIG_PV_SHIM_EXCLUSIVE)
 paging_domctl_cont                 do       do       do       do       -
 #endif
+iommu_op                           do       do       -        -        -
 
 #endif /* !CPPCHECK */
