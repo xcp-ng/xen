cb12bf3c66b85e33c92fb880eca19b55841b2904
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    For XenServer, we build cumulative live patches and replace so there is only ever a single active one. This may cause duplicate new symbol errors. Because of the lookup order (hypervisor first) and the fact that live patches will only ever be built against the hypervisor (and not another live patch), it should be safe to ignore these errors.

--- xen/tools/symbols.c
+++ xen/tools/symbols.c
@@ -597,7 +597,7 @@ static int compare_name(const void *p1, const void *p2)
 int main(int argc, char **argv)
 {
 	unsigned int i;
-	bool unsorted = false, warn_dup = false, found_dup = false;
+	bool unsorted = false, warn_dup = false, error_dup = false, found_dup = false;
 
 	if (argc >= 2) {
 		for (i = 1; i < argc; i++) {
@@ -617,6 +617,8 @@ int main(int argc, char **argv)
 				sort_by_name = 1;
 			else if (strcmp(argv[i], "--warn-dup") == 0)
 				warn_dup = true;
+			else if (strcmp(argv[i], "--error-dup") == 0)
+				warn_dup = error_dup = true;
 			else if (strcmp(argv[i], "--xensyms") == 0)
 				map_only = true;
 			else
@@ -642,7 +644,7 @@ int main(int argc, char **argv)
 		unsorted = true;
 	}
 
-	if (found_dup)
+	if (error_dup && found_dup)
 		exit(1);
 
 	if (unsorted)
