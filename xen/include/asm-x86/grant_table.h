c983e59e4c5d107e3cc4c1f0b79a557b90896cd9
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Implement an optimisation to avoid a TLB flush if the grant mapped page was not accessed whilst it was mapped.
    Signed-off-by: Malcolm Crossley <malcolm.crossley@citrix.com>
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/include/asm-x86/grant_table.h
+++ xen/include/asm-x86/grant_table.h
@@ -32,11 +32,11 @@ static inline int create_grant_host_mapping(uint64_t addr, mfn_t frame,
 
 static inline int replace_grant_host_mapping(uint64_t addr, mfn_t frame,
                                              uint64_t new_addr,
-                                             unsigned int flags)
+                                             unsigned int flags, int *page_accessed)
 {
     if ( paging_mode_external(current->domain) )
         return replace_grant_p2m_mapping(addr, frame, new_addr, flags);
-    return replace_grant_pv_mapping(addr, frame, new_addr, flags);
+    return replace_grant_pv_mapping(addr, frame, new_addr, flags, page_accessed);
 }
 
 static inline unsigned int gnttab_dom0_max(void)
