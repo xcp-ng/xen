3876408585f21124c7d82ea63cae23136e845f81
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    xen-domctl-set-privileged-domain.patch

--- xen/include/public/domctl.h
+++ xen/include/public/domctl.h
@@ -1257,6 +1257,7 @@ struct xen_domctl {
 #define XEN_DOMCTL_soft_reset                    79
 #define XEN_DOMCTL_query_disable_migrate         80
 #define XEN_DOMCTL_get_runstate_info             98
+#define XEN_DOMCTL_set_privileged                99
 #define XEN_DOMCTL_gdbsx_guestmemio            1000
 #define XEN_DOMCTL_gdbsx_pausevcpu             1001
 #define XEN_DOMCTL_gdbsx_unpausevcpu           1002
