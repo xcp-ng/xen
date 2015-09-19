d15f2135ef5a594ac88dda4b6cebd9e7af554fe2
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    ocaml-cpuid-helpers.patch

--- tools/ocaml/libs/xc/xenctrl.ml
+++ tools/ocaml/libs/xc/xenctrl.ml
@@ -286,6 +286,10 @@ type featureset_index =
 	| Featureset_pv_max
 	| Featureset_hvm_max
 external get_cpu_featureset : handle -> featureset_index -> int64 array = "stub_xc_get_cpu_featureset"
+external get_featureset : handle -> featureset_index -> int64 array = "stub_xc_get_cpu_featureset"
+
+external upgrade_oldstyle_featuremask: handle -> int64 array -> bool -> int64 array = "stub_upgrade_oldstyle_featuremask"
+external oldstyle_featuremask: handle -> int64 array = "stub_oldstyle_featuremask"
 
 external watchdog : handle -> int -> int32 -> int
   = "stub_xc_watchdog"
