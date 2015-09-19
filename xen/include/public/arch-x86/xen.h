a7330c77334d192a52ce0543dee46a457760bf78
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Toolstack support for accessing TSX_CTRL
    Partial backport of:
     * 43a9573a8ed - libx86: Introduce a helper to serialise cpuid_policy objects
     * 4874a4db383 - libx86: Introduce a helper to serialise msr_policy objects
     * 9b2d12455b1 - x86: Introduce struct cpu_policy to refer to a group of individual policies
     * 6b85e427098 - x86/sysctl: Implement XEN_SYSCTL_get_cpu_policy
    
    with a stub implemention of XEN_SYSCTL_get_cpu_policy which only responds with
    host msrs.

--- xen/include/public/arch-x86/xen.h
+++ xen/include/public/arch-x86/xen.h
@@ -314,6 +314,24 @@ struct xen_arch_domainconfig {
 #define XEN_ACPI_GPE0_CPUHP_BIT      2
 #endif
 
+/*
+ * Representations of architectural CPUID and MSR information.  Used as the
+ * serialised version of Xen's internal representation.
+ */
+typedef struct xen_cpuid_leaf {
+#define XEN_CPUID_NO_SUBLEAF 0xffffffffu
+    uint32_t leaf, subleaf;
+    uint32_t a, b, c, d;
+} xen_cpuid_leaf_t;
+DEFINE_XEN_GUEST_HANDLE(xen_cpuid_leaf_t);
+
+typedef struct xen_msr_entry {
+    uint32_t idx;
+    uint32_t flags; /* Reserved MBZ. */
+    uint64_t val;
+} xen_msr_entry_t;
+DEFINE_XEN_GUEST_HANDLE(xen_msr_entry_t);
+
 #endif /* !__ASSEMBLY__ */
 
 /*
