b4b0b9959f3c84518fa681512e86e3c1e22c6d78
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    add-pv-iommu-foreign-support.patch

--- xen/include/xsm/dummy.h
+++ xen/include/xsm/dummy.h
@@ -619,6 +619,12 @@ static XSM_INLINE int xsm_vm_event_control(
     return xsm_default_action(action, current->domain, d);
 }
 
+static XSM_INLINE int xsm_iommu_control(XSM_DEFAULT_ARG struct domain *d, unsigned long op)
+{
+    XSM_ASSERT_ACTION(XSM_DM_PRIV);
+    return xsm_default_action(action, current->domain, d);
+}
+
 #ifdef CONFIG_MEM_ACCESS
 static XSM_INLINE int xsm_mem_access(XSM_DEFAULT_ARG struct domain *d)
 {
