1afd2968fc3f1f0577c7f6c8e6b93b2414de9df6
Ross Lagerwall <ross.lagerwall@citrix.com>
2015-09-19 06:09:42 +0000

    Skip call to flask
    XenServer uses silo mode rather than flask so the toolstack should not attempt
    to blindly make flask calls. This causes noise from the hypercall filter layer
    in the dom0 kernel since xsm_op is blocked.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- tools/libs/light/libxl_domain.c
+++ tools/libs/light/libxl_domain.c
@@ -276,15 +276,12 @@ void libxl__xcinfo2xlinfo(libxl_ctx *ctx,
                           const xc_domaininfo_t *xcinfo,
                           libxl_dominfo *xlinfo)
 {
-    size_t size;
     int saved_errno = errno;
 
     memcpy(&(xlinfo->uuid), xcinfo->handle, sizeof(xen_domain_handle_t));
     xlinfo->domid = xcinfo->domain;
     xlinfo->ssidref = xcinfo->ssidref;
-    if (libxl_flask_sid_to_context(ctx, xlinfo->ssidref,
-                                   &xlinfo->ssid_label, &size) < 0)
-        xlinfo->ssid_label = NULL;
+    xlinfo->ssid_label = NULL;
     errno = saved_errno;
 
     xlinfo->dying      = !!(xcinfo->flags&XEN_DOMINF_dying);
