3cf16ec7d774dd320e9a99ac9b90df861f37cd0b
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    xen-4.13.1-insert-Ice-Lake-and-Comet-Lake-model-numbers.backport.patch

--- xen/arch/x86/acpi/cpu_idle.c
+++ xen/arch/x86/acpi/cpu_idle.c
@@ -179,11 +179,17 @@ static void do_get_hw_residencies(void *arg)
     case 0x4E:
     case 0x55:
     case 0x5E:
+    /* Ice Lake */
+    case 0x7D:
+    case 0x7E:
     /* Cannon Lake */
     case 0x66:
     /* Kaby Lake */
     case 0x8E:
     case 0x9E:
+    /* Comet Lake */
+    case 0xA5:
+    case 0xA6:
         GET_PC2_RES(hw_res->pc2);
         GET_CC7_RES(hw_res->cc7);
         /* fall through */
