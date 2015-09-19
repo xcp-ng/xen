0f9e77d1f5fb972c8d8da71fd694bf3b05371b3e
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Quiet "Broke affinity for irq" messages during shutdown
    Quiet the "Broke affinity for irq" messages during a shutdown or
    suspend, as it is already done during a reboot. Do this by conditionally
    printing the messages depending on the system state, as originally
    suggested by Jan when discussing commit e509b8e09c00 ("x86: fixup IRQs
    when CPUs go down during shutdown").
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Suggested-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/irq.c
+++ xen/arch/x86/irq.c
@@ -2533,7 +2533,7 @@ static int __init setup_dump_irqs(void)
 __initcall(setup_dump_irqs);
 
 /* Reset irq affinities to match the given CPU mask. */
-void fixup_irqs(const cpumask_t *mask, bool verbose)
+void fixup_irqs(const cpumask_t *mask)
 {
     unsigned int irq;
     static int warned;
@@ -2637,7 +2637,8 @@ void fixup_irqs(const cpumask_t *mask, bool verbose)
 
         spin_unlock(&desc->lock);
 
-        if ( !verbose )
+        if ( system_state == SYS_STATE_reboot ||
+             system_state == SYS_STATE_suspend )
             continue;
 
         if ( !set_affinity )
