234ec6399ac017ea3b63bf723900f4ba5db6307a
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 07:09:42 +0100

    XenServer changes to Xen command line default options.
    * fatal nmis even in regular builds.  dom0 is not generally capable of dealing
      with nmis it gets, causing serious errors to be obscured.
    
    * forceful watchdog by default.  Should allow IPMI "inject NMI" options on
      Dell/IBM/SuperMicro servers to work in combination with the watchdog.
    
    * disable APICv.  Temporary until we can debug why it causes loss of VM
      interrupts when migrating between APICv and non APICv-capable hardware.
    
    * Console timestamps (Linux style)
    
    * Console ring size bumped up to 64k, as well as dom0 tools like
      libxl, and the ocaml and python bindings.
    
    * Assume UC mappings for unknown EFI cacheability (firmware workarounds)
    
    * Always probe for RTC if the the ACPI tables claim it isn't present.  HP Gen9
      servers erroneously claim that an RTC is not present, which breaks legacy
      boot.
    
    * Increase default number of guest PIRQs. Bring the default number of PIRQs
      from 32 to 64.
    
    * Allow dom0 to create cached mappings of reserved regions.
    
    * Disallow PV linear pagetables by default.
    
    * XSA-304: fast by default, allow executable superpages.
    
    * Always scrub domheap pages.  Don't rely on guests getting this right.

--- xen/common/efi/boot.c
+++ xen/common/efi/boot.c
@@ -1553,7 +1553,7 @@ efi_start(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
 #include <asm/spec_ctrl.h>
 
-static bool __initdata efi_map_uc;
+static bool __initdata efi_map_uc = true;
 
 static int __init cf_check parse_efi_param(const char *s)
 {
