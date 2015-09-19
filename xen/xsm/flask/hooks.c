3a13c3efb639f5951fe2b39cbf401c99065ad737
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Collect domain runstate information in the hypervisor, and export through a domctl operation.

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -748,6 +748,9 @@ static int flask_domctl(struct domain *d, int cmd)
     case XEN_DOMCTL_soft_reset:
         return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__SOFT_RESET);
 
+    case XEN_DOMCTL_get_runstate_info:
+        return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__GET_RUNSTATE_INFO);
+
     default:
         return avc_unknown_permission("domctl", cmd);
     }
