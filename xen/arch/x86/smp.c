ffc1ba80286a1744a123f346c04287c880d4302d
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Quiet "Broke affinity for irq" messages during shutdown
    Quiet the "Broke affinity for irq" messages during a shutdown or
    suspend, as it is already done during a reboot. Do this by conditionally
    printing the messages depending on the system state, as originally
    suggested by Jan when discussing commit e509b8e09c00 ("x86: fixup IRQs
    when CPUs go down during shutdown").
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Suggested-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/smp.c
+++ xen/arch/x86/smp.c
@@ -305,7 +305,7 @@ void smp_send_stop(void)
     int timeout = 10;
 
     local_irq_disable();
-    fixup_irqs(cpumask_of(smp_processor_id()), 0);
+    fixup_irqs(cpumask_of(smp_processor_id()));
     local_irq_enable();
 
     smp_call_function(stop_this_cpu, NULL, 0);
