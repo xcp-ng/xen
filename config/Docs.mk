d5270c1da1e8ea65312a529d82a5f9e8675f5b5e
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Various configuration files.
    Most notibly, the cached results of
    
    WGET=/bin/false FETCHER=/bin/false \
    ./configure --prefix=/usr \
                --libdir=/usr/lib64 \
                --libexecdir=/usr/libexec \
                --disable-seabios \
                --disable-stubdom \
                --disable-xsmpolicy \
                --enable-systemd \
                --with-xenstored=oxenstored \
                --with-system-qemu=/usr/lib64/xen/bin/qemu-system-i386 \
                --with-system-ipxe=/usr/share/ipxe/ipxe.bin \
                --with-system-ovmf=/usr/share/edk2/OVMF.fd

---
+++ config/Docs.mk
@@ -0,0 +1,10 @@
+-include $(XEN_ROOT)/config/Paths.mk
+
+# Tools
+FIG2DEV             := 
+POD2MAN             := /usr/bin/pod2man
+POD2HTML            := /usr/bin/pod2html
+POD2TEXT            := /usr/bin/pod2text
+MARKDOWN            := /usr/bin/markdown_py
+PANDOC              := /usr/bin/pandoc
+PERL                := /usr/bin/perl
