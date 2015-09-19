3362300b6a77c6b7277576dfff5857e56a97e971
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    upstream-pv-iommu-tools.patch

--- tools/libxc/include/xenctrl.h
+++ tools/libxc/include/xenctrl.h
@@ -47,6 +47,7 @@
 #include <xen/tmem.h>
 #include <xen/kexec.h>
 #include <xen/platform.h>
+#include <xen/pv-iommu.h>
 
 #include "xentoollog.h"
 
@@ -2477,6 +2478,9 @@ int xc_kexec_unload(xc_interface *xch, int type);
 int xc_kexec_status(xc_interface *xch, int type);
 
 typedef xenpf_resource_entry_t xc_resource_entry_t;
+
+
+int xc_iommu_op(xc_interface *xch, struct pv_iommu_op *ops, unsigned int count);
 
 /*
  * Generic resource operation which contains multiple non-preemptible
