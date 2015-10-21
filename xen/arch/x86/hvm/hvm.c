27c6ca2a59f4263c3df284c43610926d8042c160
Andrew Cooper <andrew.cooper3@citrix.com>
2015-10-21 22:24:34 +0100

    x86/PoD: Command line option to prohibit any PoD operations
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/hvm/hvm.c
+++ xen/arch/x86/hvm/hvm.c
@@ -97,6 +97,9 @@ unsigned long __section(".bss.page_aligned") __aligned(PAGE_SIZE)
 static bool_t __initdata opt_hap_enabled = 1;
 boolean_param("hap", opt_hap_enabled);
 
+bool opt_pod_enabled = true;
+boolean_param("pod", opt_pod_enabled);
+
 #ifndef opt_hvm_fep
 /* Permit use of the Forced Emulation Prefix in HVM guests */
 bool_t __read_mostly opt_hvm_fep;
