f5fa6f8a6c9d58218f76213e1e4492ba4514a8b0
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-04-26 13:59:38 +0100

    efi: Workaround page fault during runtime service
    Some hardware makes use of memory of type EfiACPIMemoryNVS during the
    ResetSystem runtime service but does not mark the memory as needing a
    runtime mapping causing a fatal page fault.
    
    To workaround this, map this type of memory for runtime services
    regardless of whether it marked with EFI_MEMORY_RUNTIME.
    
    The hardware on which this behavior was observed:
    Dell OptiPlex 9020
    Firmware version: A15
    Firmware release date: 11/08/2015
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- xen/common/efi/boot.c
+++ xen/common/efi/boot.c
@@ -100,6 +100,7 @@ static SIMPLE_TEXT_OUTPUT_INTERFACE *__initdata StdErr;
 static UINT32 __initdata mdesc_ver;
 static bool_t __initdata map_bs;
 static bool_t __initdata map_res = 1;
+static bool_t __initdata map_nvs = 1;
 
 static struct file __initdata cfg;
 static struct file __initdata kernel;
@@ -1281,6 +1282,8 @@ void __init efi_init_memory(void)
               (!map_bs ||
                (desc->Type != EfiBootServicesCode &&
                 desc->Type != EfiBootServicesData)) &&
+              (!map_nvs ||
+               desc->Type != EfiACPIMemoryNVS) &&
               (!map_res ||
                desc->Type != EfiReservedMemoryType)) )
             continue;
