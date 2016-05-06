3531190fd7340deba427d691991b0f7d16cb6ee4
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-05-06 15:02:45 +0100

    lib: Add a generic implementation of current_text_addr()
    Remove the unused x86 implementation.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- xen/include/xen/lib.h
+++ xen/include/xen/lib.h
@@ -191,6 +191,8 @@ void dump_execstate(struct cpu_user_regs *);
 
 void init_constructors(void);
 
+void *current_text_addr(void);
+
 void *bsearch(const void *key, const void *base, size_t num, size_t size,
               int (*cmp)(const void *key, const void *elt));
 
