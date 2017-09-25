a5ed08e0a482d12ad31e8113c2124afa46702b91
Sergey Dyasli <sergey.dyasli@citrix.com>
2017-09-25 10:53:00 +0200

    x86/msr: introduce struct msr_domain_policy
    The new structure contains information about guest's MSRs that are
    shared between all domain's vCPUs. It starts with only 1 MSR:
    
        MSR_INTEL_PLATFORM_INFO
    
    Which currently has only 1 usable bit: cpuid_faulting.
    
    Add 2 global policy objects: hvm_max and pv_max that are inited during
    boot up. It's always possible to emulate CPUID faulting for HVM guests
    while for PV guests the H/W support is required.
    
    Add init_domain_msr_policy() which sets initial MSR policy during
    domain creation with a special case for Dom0.
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- xen/arch/x86/domain.c
+++ xen/arch/x86/domain.c
@@ -552,6 +552,7 @@ int arch_domain_create(struct domain *d, unsigned int domcr_flags,
     if ( is_idle_domain(d) )
     {
         d->arch.emulation_flags = 0;
+        d->arch.msr = ZERO_BLOCK_PTR; /* Catch stray misuses. */
     }
     else
     {
@@ -633,6 +634,9 @@ int arch_domain_create(struct domain *d, unsigned int domcr_flags,
         d->arch.x86        = boot_cpu_data.x86;
         d->arch.x86_model  = boot_cpu_data.x86_model;
 
+        if ( (rc = init_domain_msr_policy(d)) )
+            goto fail;
+
         d->arch.ioport_caps = 
             rangeset_new(d, "I/O Ports", RANGESETF_prettyprint_hex);
         rc = -ENOMEM;
@@ -692,6 +696,7 @@ int arch_domain_create(struct domain *d, unsigned int domcr_flags,
     cleanup_domain_irq_mapping(d);
     free_xenheap_page(d->shared_info);
     xfree(d->arch.cpuids);
+    xfree(d->arch.msr);
     if ( paging_initialised )
         paging_final_teardown(d);
     free_perdomain_mappings(d);
@@ -710,6 +715,7 @@ void arch_domain_destroy(struct domain *d)
 
     xfree(d->arch.e820);
     xfree(d->arch.cpuids);
+    xfree(d->arch.msr);
 
     free_domain_pirqs(d);
     if ( !is_idle_domain(d) )
