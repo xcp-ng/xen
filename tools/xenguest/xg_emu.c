a2c17ed5ba9f10ed3bcd1c37af34951b523a973e
Vates Git Importer <gitimporter@vates.tech>
2018-02-24 23:51:42 +0100

    rpmbuild: remaining diff.
    Signed-off-by: Vates Git Importer <gitimporter@vates.tech>

--- tools/xenguest/xg_emu.c
+++ tools/xenguest/xg_emu.c
@@ -16,7 +16,6 @@ static int stream_fd = -1;
 
 static int arg_store_port = -1;
 static int arg_console_port = -1;
-static int arg_mem_pnode = -1;
 
 /* timeout in seconds */
 #define COMMAND_TIMEOUT (60 * 2)
@@ -196,8 +195,6 @@ static void do_cmd_restore(emp_call_args *args)
 
     emp_send_return(args->cli, NULL);
 
-    opt_mem_pnode = (arg_mem_pnode >= 0) ? (unsigned int)arg_mem_pnode
-                                         : XC_NUMA_NO_NODE;
     stub_xc_domain_restore(stream_fd, arg_store_port, arg_console_port, !pv_mode,
                            &store_mfn, &console_mfn);
 
@@ -233,7 +230,6 @@ struct arg_list
 const static struct arg_list setable_args[] = {
     {"store_port",   int_type,  .a_int = &arg_store_port},
     {"console_port", int_type,  .a_int = &arg_console_port},
-    {"mem_pnode",    int_type,  .a_int = &arg_mem_pnode},
     {"pv",           bool_type, .a_bool = &pv_mode},
     {"vgpu",         bool_type, .a_bool = &opt_vgpu},
     {}
