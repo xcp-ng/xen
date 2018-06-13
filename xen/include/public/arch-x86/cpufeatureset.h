3e6d9ebe8f5f8a4d5b0287e6ed961fc783b29224
Yang Zhong <yang.zhong@intel.com>
2018-06-13 14:21:24 +0800

    x86/cpuid: Enable new SSE/AVX/AVX512 cpu features
    Intel IceLake cpu has added new cpu features: AVX512VBMI2/GFNI/
    VAES/AVX512VNNI/AVX512BITALG/VPCLMULQDQ. Those new cpu features
    need expose to guest.
    
    The bit definition:
    CPUID.(EAX=7,ECX=0):ECX[bit 06] AVX512VBMI2
    CPUID.(EAX=7,ECX=0):ECX[bit 08] GFNI
    CPUID.(EAX=7,ECX=0):ECX[bit 09] VAES
    CPUID.(EAX=7,ECX=0):ECX[bit 10] VPCLMULQDQ
    CPUID.(EAX=7,ECX=0):ECX[bit 11] AVX512VNNI
    CPUID.(EAX=7,ECX=0):ECX[bit 12] AVX512_BITALG
    
    The release document ref below link:
    https://software.intel.com/sites/default/files/managed/c5/15/\
    architecture-instruction-set-extensions-programming-reference.pdf
    
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>

--- xen/include/public/arch-x86/cpufeatureset.h
+++ xen/include/public/arch-x86/cpufeatureset.h
@@ -228,6 +228,9 @@ XEN_CPUFEATURE(AVX512VBMI,    6*32+ 1) /*A  AVX-512 Vector Byte Manipulation Ins
 XEN_CPUFEATURE(UMIP,          6*32+ 2) /*S  User Mode Instruction Prevention */
 XEN_CPUFEATURE(PKU,           6*32+ 3) /*H  Protection Keys for Userspace */
 XEN_CPUFEATURE(OSPKE,         6*32+ 4) /*!  OS Protection Keys Enable */
+XEN_CPUFEATURE(AVX512_VBMI2,  6*32+ 6) /*A  Additional AVX-512 VBMI Instructions */
+XEN_CPUFEATURE(AVX512_VNNI,   6*32+ 11) /*A  Vector Neural Network Instructions */
+XEN_CPUFEATURE(AVX512_BITALG, 6*32+ 12) /*A  Support for VPOPCNT[B,W] and VPSHUFBITQMB */
 XEN_CPUFEATURE(AVX512_VPOPCNTDQ, 6*32+14) /*A  POPCNT for vectors of DW/QW */
 XEN_CPUFEATURE(RDPID,         6*32+22) /*A  RDPID instruction */
 
