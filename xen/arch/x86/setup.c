7272b74c3e09ce7ac368648b931666acb514d6a7
Sergey Dyasli <sergey.dyasli@citrix.com>
2018-06-26 09:39:27 +0100

    Hide AVX-512 from guests by default
    It can be enabled by providing "cpuid=avx512" in Xen's cmdline.
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -106,6 +106,8 @@ unsigned long __initdata highmem_start;
 size_param("highmem-start", highmem_start);
 #endif
 
+extern bool __initdata opt_avx512;
+
 cpumask_t __read_mostly cpu_present_map;
 
 unsigned long __read_mostly xen_phys_start;
@@ -1469,6 +1471,9 @@ void __init noreturn __start_xen(unsigned long mbi_p)
 
     if ( opt_invpcid && cpu_has_invpcid )
         use_invpcid = 1;
+
+    if ( !opt_avx512 )
+        setup_clear_cpu_cap(X86_FEATURE_AVX512F);
 
     init_speculation_mitigations();
 
