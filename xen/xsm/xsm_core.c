77447ddf17d1fba60ceed193f5a4baa2f7a63f53
Xin Li <xin.li@citrix.com>
2018-06-29 16:30:10 +0800

    xen/xsm: Add new SILO mode for XSM
    When SILO is enabled, there would be no page-sharing or event notifications
    between unprivileged VMs (no grant tables or event channels).
    
    Signed-off-by: Xin Li <xin.li@citrix.com>

--- xen/xsm/xsm_core.c
+++ xen/xsm/xsm_core.c
@@ -34,6 +34,7 @@ struct xsm_operations *xsm_ops;
 enum xsm_bootparam {
     XSM_BOOTPARAM_DUMMY,
     XSM_BOOTPARAM_FLASK,
+    XSM_BOOTPARAM_SILO,
 };
 
 static enum xsm_bootparam __initdata xsm_bootparam = XSM_BOOTPARAM_DUMMY;
@@ -47,6 +48,10 @@ static int __init parse_xsm_param(const char *s)
 #ifdef CONFIG_XSM_FLASK
     else if ( !strcmp(s, "flask") )
         xsm_bootparam = XSM_BOOTPARAM_FLASK;
+#endif
+#ifdef CONFIG_XSM_SILO
+    else if ( !strcmp(s, "silo") )
+        xsm_bootparam = XSM_BOOTPARAM_SILO;
 #endif
     else {
         printk("XSM: can't parse boot parameter xsm=%s\n", s);
@@ -91,6 +96,10 @@ static int __init xsm_core_init(const void *policy_buffer, size_t policy_size)
 
     case XSM_BOOTPARAM_FLASK:
         flask_init(policy_buffer, policy_size);
+        break;
+
+    case XSM_BOOTPARAM_SILO:
+        silo_init();
         break;
 
     default:
