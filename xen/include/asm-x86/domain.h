c52d86a8320469ce857a590b9ab8a1061738f9b2
Juergen Gross <jgross@suse.com>
2018-07-23 07:11:40 +0100

    x86/shadow: Infrastructure to force a PV guest into shadow mode
    To mitigate L1TF, we cannot alter an architecturally-legitimate PTE a PV guest
    chooses to write, but we can force the PV domain into shadow mode so Xen
    controls the PTEs which are reachable by the CPU pagewalk.
    
    Introduce new shadow mode, PG_SH_forced, and a tasklet to perform the
    transition.  Later patches will introduce the logic to enable this mode at the
    appropriate time.
    
    To simplify vcpu cleanup, make tasklet_kill() idempotent with respect to
    tasklet_init(), which involves adding a helper to check for an uninitialised
    list head.
    
    This is part of XSA-273 / CVE-2018-3620.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Tim Deegan <tim@xen.org>

--- xen/include/asm-x86/domain.h
+++ xen/include/asm-x86/domain.h
@@ -121,6 +121,9 @@ struct shadow_domain {
 
     /* Has this domain ever used HVMOP_pagetable_dying? */
     bool_t pagetable_dying_op;
+
+    /* PV L1 Terminal Fault mitigation. */
+    struct tasklet pv_l1tf_tasklet;
 #endif
 };
 
@@ -259,6 +262,8 @@ struct pv_domain
 
     /* XPTI active? */
     bool xpti;
+    /* Mitigate L1TF with shadow/crashing? */
+    bool_t check_l1tf;
 
     /* map_domain_page() mapping cache. */
     struct mapcache_domain mapcache;
