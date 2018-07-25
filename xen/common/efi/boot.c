42c8221a4c120bdf7ceb95b2f4a115d2afd4a08d
Andrew Cooper <andrew.cooper3@citrix.com>
2018-07-25 12:10:19 +0000

    x86/spec-ctrl: Calculate safe PTE addresses for L1TF mitigations
    Safe PTE addresses for L1TF mitigations are ones which are within the L1D
    address width (may be wider than reported in CPUID), and above the highest
    cacheable RAM/NVDIMM/BAR/etc.
    
    All logic here is best-effort heuristics, which should in practice be fine for
    most hardware.  Future work will see about disentangling the SRAT handling
    further, as well as having L0 pass this information down to lower levels when
    virtualised.
    
    This is part of XSA-273 / CVE-2018-3620.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>

--- xen/common/efi/boot.c
+++ xen/common/efi/boot.c
@@ -1155,6 +1155,7 @@ efi_start(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
 static bool_t __initdata efi_rs_enable = 1;
 static bool_t __initdata efi_map_uc;
+#include <asm/spec_ctrl.h>
 
 static void __init parse_efi_param(char *s)
 {
@@ -1253,6 +1254,16 @@ void __init efi_init_memory(void)
                            " type=%u attr=%016" PRIx64 "\n",
                desc->PhysicalStart, desc->PhysicalStart + len - 1,
                desc->Type, desc->Attribute);
+
+        if ( (desc->Attribute & (EFI_MEMORY_WB | EFI_MEMORY_WT)) ||
+             (efi_bs_revision >= EFI_REVISION(2, 5) &&
+              (desc->Attribute & EFI_MEMORY_WP)) )
+        {
+            /* Supplement the heuristics in l1tf_calculations(). */
+            l1tf_safe_maddr =
+                max(l1tf_safe_maddr,
+                    ROUNDUP(desc->PhysicalStart + len, PAGE_SIZE));
+        }
 
         if ( !efi_rs_enable ||
              (!(desc->Attribute & EFI_MEMORY_RUNTIME) &&
