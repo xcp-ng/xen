6d3e8c0be9241e909b2bb65472fb14d4cd5d7081
Sergey Dyasli <sergey.dyasli@citrix.com>
2018-11-07 09:31:17 +0000

    Allocate space in structs pre-emptively to increase chances of livepatching
    1. Expand FEATURESET for 4 more words
    2. Increment CPUID_GUEST_NR_FEAT to get an additional feature leaf
       in struct cpuid_policy
    3. Reserve space in the following structs:
    
        grant_table
        vtd_iommu
        amd_iommu
        guest_iommu
        hvm_ioreq_vcpu
        hvm_ioreq_server
        msr_domain_policy
        msr_vcpu_policy
        p2m_domain
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/drivers/passthrough/vtd/iommu.h
+++ xen/drivers/passthrough/vtd/iommu.h
@@ -539,6 +539,9 @@ struct vtd_iommu {
     unsigned long *domid_bitmap;  /* domain id bitmap */
     u16 *domid_map;               /* domain id mapping array */
     uint32_t version;
+
+    /* Reserved space */
+    uint64_t _rsvd_livepatch[2];
 };
 
 #define INTEL_IOMMU_DEBUG(fmt, args...) \
