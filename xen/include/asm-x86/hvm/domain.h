5c05381f65768cda181467a8f6c1cb6b3dd66bc5
Sergey Dyasli <sergey.dyasli@citrix.com>
2018-11-07 09:31:17 +0000

    Allocate space in structs pre-emptively to increase chances of livepatching
    1. Expand FEATURESET for 4 more words
    2. Increment CPUID_GUEST_NR_FEAT to get an additional feature leaf
       in struct cpuid_policy
    3. Reserve space in the following structs:
    
        grant_table
        vtd_iommu
        amd_iommu
        guest_iommu
        hvm_ioreq_vcpu
        hvm_ioreq_server
        msr_domain_policy
        msr_vcpu_policy
        p2m_domain
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/include/asm-x86/hvm/domain.h
+++ xen/include/asm-x86/hvm/domain.h
@@ -46,6 +46,9 @@ struct hvm_ioreq_vcpu {
     struct vcpu      *vcpu;
     evtchn_port_t    ioreq_evtchn;
     bool             pending;
+
+    /* Reserved space */
+    uint64_t _rsvd_livepatch[1];
 };
 
 #define NR_IO_RANGE_TYPES (XEN_DMOP_IO_RANGE_PCI + 1)
@@ -67,6 +70,9 @@ struct hvm_ioreq_server {
     struct rangeset        *range[NR_IO_RANGE_TYPES];
     bool                   enabled;
     uint8_t                bufioreq_handling;
+
+    /* Reserved space */
+    uint64_t _rsvd_livepatch[2];
 };
 
 /*
