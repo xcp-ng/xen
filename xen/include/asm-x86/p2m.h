cd1cc8f5fe9ce7e04c9e6f82e2ca15744267c192
Sergey Dyasli <sergey.dyasli@citrix.com>
2018-11-07 09:31:17 +0000

    Allocate space in structs pre-emptively to increase chances of livepatching
    1. Expand FEATURESET for 4 more words
    2. Increment CPUID_GUEST_NR_FEAT to get an additional feature leaf
       in struct cpuid_policy
    3. Reserve space in the following structs:
    
        grant_table
        intel_iommu
        iommu
        amd_iommu
        guest_iommu
        hvm_ioreq_vcpu
        hvm_ioreq_server
        msr_domain_policy
        msr_vcpu_policy
        p2m_domain
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/include/asm-x86/p2m.h
+++ xen/include/asm-x86/p2m.h
@@ -356,6 +356,9 @@ struct p2m_domain {
          unsigned int flags;
          unsigned long entry_count;
      } ioreq;
+
+    /* Reserved space */
+    uint64_t _rsvd_livepatch[2];
 };
 
 /* get host p2m table */
