c9cf90cbe0366f21e3b90c56735865e083e281fd
Razvan Cojocaru <rcojocaru@bitdefender.com>
2018-11-15 13:35:08 +0100

    x86/altp2m: propagate ept.ad changes to all active altp2ms
    This patch is a pre-requisite for fixing the logdirty VGA issue
    (display freezes when switching to a new altp2m view early in a
    domain's lifetime).
    
    The new ept_set_ad_sync() function has been added to update all
    active altp2ms' ept.ad. New altp2ms will inherit the hostp2m's
    ept.ad value.
    
    The p2m_{en,dis}able_hardware_log_dirty() hostp2m locking has
    been moved to the new ept_{en,dis}able_hardware_log_dirty()
    functions as part of the code refactoring, while locks for the
    individual altp2ms are taken in ept_set_ad_sync() (called by
    ept_{en,dis}able_pml()).
    
    Suggested-by: George Dunlap <george.dunlap@citrix.com>
    Signed-off-by: Razvan Cojocaru <rcojocaru@bitdefender.com>
    Tested-by: Tamas K Lengyel <tamas@tklengyel.com>
    Acked-by: George Dunlap <george.dunlap@citrix.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- xen/arch/x86/mm/p2m.c
+++ xen/arch/x86/mm/p2m.c
@@ -354,11 +354,7 @@ void p2m_enable_hardware_log_dirty(struct domain *d)
     struct p2m_domain *p2m = p2m_get_hostp2m(d);
 
     if ( p2m->enable_hardware_log_dirty )
-    {
-        p2m_lock(p2m);
         p2m->enable_hardware_log_dirty(p2m);
-        p2m_unlock(p2m);
-    }
 }
 
 void p2m_disable_hardware_log_dirty(struct domain *d)
@@ -366,11 +362,7 @@ void p2m_disable_hardware_log_dirty(struct domain *d)
     struct p2m_domain *p2m = p2m_get_hostp2m(d);
 
     if ( p2m->disable_hardware_log_dirty )
-    {
-        p2m_lock(p2m);
         p2m->disable_hardware_log_dirty(p2m);
-        p2m_unlock(p2m);
-    }
 }
 
 void p2m_flush_hardware_cached_dirty(struct domain *d)
