94c40b87a9591176b1b0bf824269ca86ddd5a23f
Sergey Dyasli <sergey.dyasli@citrix.com>
2018-11-21 16:12:45 +0000

    x86/pv: silently discard writes into MSR_AMD64_LS_CFG
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/arch/x86/pv/emul-priv-op.c
+++ xen/arch/x86/pv/emul-priv-op.c
@@ -1030,6 +1030,10 @@ static int write_msr(unsigned int reg, uint64_t val,
             return X86EMUL_OKAY;
         break;
 
+    case MSR_AMD64_LS_CFG:
+        /* Silently discard the write to avoid gdprintk() spam from below */
+        break;
+
     case MSR_P6_PERFCTR(0) ... MSR_P6_PERFCTR(7):
     case MSR_P6_EVNTSEL(0) ... MSR_P6_EVNTSEL(3):
     case MSR_CORE_PERF_FIXED_CTR0 ... MSR_CORE_PERF_FIXED_CTR2:
