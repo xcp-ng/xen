003b7edd7eeb4fd2bdca2c4a3867848dc39c70db
Paul Durrant <paul.durrant@citrix.com>
2018-12-20 13:36:24 +0000

    viridian: add missing context save helpers into synic and time modules
    Currently the time module lacks vcpu context save helpers and the synic
    module lacks domain context save helpers. These helpers are not yet
    required but subsequent patches will require at least some of them so this
    patch completes the set to avoid introducing them in an ad-hoc way.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>

--- xen/arch/x86/hvm/viridian/time.c
+++ xen/arch/x86/hvm/viridian/time.c
@@ -232,6 +232,16 @@ void viridian_time_domain_deinit(struct domain *d)
 {
 }
 
+void viridian_time_save_vcpu_ctxt(
+    const struct vcpu *v, struct hvm_viridian_vcpu_context *ctxt)
+{
+}
+
+void viridian_time_load_vcpu_ctxt(
+    struct vcpu *v, const struct hvm_viridian_vcpu_context *ctxt)
+{
+}
+
 void viridian_time_save_domain_ctxt(
     const struct domain *d, struct hvm_viridian_domain_context *ctxt)
 {
