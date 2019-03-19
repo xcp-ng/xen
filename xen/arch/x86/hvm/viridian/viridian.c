cc6c9b0c58244472d5a2236dbe30b7e177c79fa9
Paul Durrant <paul.durrant@citrix.com>
2019-03-19 16:29:00 +0100

    viridian: add implementation of synthetic timers
    This patch introduces an implementation of the STIMER0-15_CONFIG/COUNT MSRs
    and hence a the first SynIC message source.
    
    The new (and documented) 'stimer' viridian enlightenment group may be
    specified to enable this feature.
    
    While in the neighbourhood, this patch adds a missing check for an
    attempt to write the time reference count MSR, which should result in an
    exception (but not be reported as an unimplemented MSR).
    
    NOTE: It is necessary for correct operation that timer expiration and
          message delivery time-stamping use the same time source as the guest.
          The specification is ambiguous but testing with a Windows 10 1803
          guest has shown that using the partition reference counter as a
          source whilst the guest is using RDTSC and the reference tsc page
          does not work correctly. Therefore the time_now() function is used.
          This implements the algorithm for acquiring partition reference time
          that is documented in the specifiction.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/hvm/viridian/viridian.c
+++ xen/arch/x86/hvm/viridian/viridian.c
@@ -184,6 +184,8 @@ void cpuid_viridian_leaves(const struct vcpu *v, uint32_t leaf,
             mask.AccessPartitionReferenceTsc = 1;
         if ( viridian_feature_mask(d) & HVMPV_synic )
             mask.AccessSynicRegs = 1;
+        if ( viridian_feature_mask(d) & HVMPV_stimer )
+            mask.AccessSyntheticTimerRegs = 1;
 
         u.mask = mask;
 
@@ -325,6 +327,8 @@ static int guest_wrmsr_viridian(struct vcpu *v, uint32_t idx, uint64_t val)
     case HV_X64_MSR_TSC_FREQUENCY:
     case HV_X64_MSR_APIC_FREQUENCY:
     case HV_X64_MSR_REFERENCE_TSC:
+    case HV_X64_MSR_TIME_REF_COUNT:
+    case HV_X64_MSR_STIMER0_CONFIG ... HV_X64_MSR_STIMER3_COUNT:
         return viridian_time_wrmsr(v, idx, val);
 
     case HV_X64_MSR_CRASH_P0:
@@ -420,6 +424,7 @@ static int guest_rdmsr_viridian(const struct vcpu *v, uint32_t idx,
     case HV_X64_MSR_APIC_FREQUENCY:
     case HV_X64_MSR_REFERENCE_TSC:
     case HV_X64_MSR_TIME_REF_COUNT:
+    case HV_X64_MSR_STIMER0_CONFIG ... HV_X64_MSR_STIMER3_COUNT:
         return viridian_time_rdmsr(v, idx, val);
 
     case HV_X64_MSR_CRASH_P0:
