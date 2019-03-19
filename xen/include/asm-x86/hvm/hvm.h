54801eee1d4477bc4b6051d702990973c7e3c318
Paul Durrant <paul.durrant@citrix.com>
2019-03-19 16:25:00 +0100

    viridian: add implementation of synthetic interrupt MSRs
    This patch introduces an implementation of the SCONTROL, SVERSION, SIEFP,
    SIMP, EOM and SINT0-15 SynIC MSRs. No message source is added and, as such,
    nothing will yet generate a synthetic interrupt. A subsequent patch will
    add an implementation of synthetic timers which will need the infrastructure
    added by this patch to deliver expiry messages to the guest.
    
    NOTE: A 'synic' option is added to the toolstack viridian enlightenments
          enumeration but is deliberately not documented as enabling these
          SynIC registers without a message source is only useful for
          debugging.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/include/asm-x86/hvm/hvm.h
+++ xen/include/asm-x86/hvm/hvm.h
@@ -404,6 +404,9 @@ bool hvm_set_guest_bndcfgs(struct vcpu *v, u64 val);
 #define has_viridian_apic_assist(d) \
     (is_viridian_domain(d) && (viridian_feature_mask(d) & HVMPV_apic_assist))
 
+#define has_viridian_synic(d) \
+    (is_viridian_domain(d) && (viridian_feature_mask(d) & HVMPV_synic))
+
 bool hvm_check_cpuid_faulting(struct vcpu *v);
 void hvm_migrate_timers(struct vcpu *v);
 void hvm_do_resume(struct vcpu *v);
