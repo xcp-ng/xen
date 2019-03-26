88e4e2e10ab8effc6a5bdd48804317c4bd873798
Sergey Dyasli <sergey.dyasli@citrix.com>
2019-03-26 09:53:25 +0000

    x86: add XEN_SYSCTL_spec_ctrl
    stop_machine infrastructure is copied from live ucode update.
    CPUID level 0x00000007:0.edx is updated on each cpu.
    CPUID policies are recalculated if there are any CPUID changes.
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/include/public/sysctl.h
+++ xen/include/public/sysctl.h
@@ -1033,6 +1033,11 @@ typedef struct xen_sysctl_cpu_policy xen_sysctl_cpu_policy_t;
 DEFINE_XEN_GUEST_HANDLE(xen_sysctl_cpu_policy_t);
 #endif
 
+struct xen_sysctl_spec_ctrl {
+#define XENPF_spec_ctrl_update  0 /* Try to use new mitigations */
+    uint32_t op;      /* IN */
+};
+
 struct xen_sysctl {
     uint32_t cmd;
 #define XEN_SYSCTL_readconsole                    1
@@ -1063,6 +1068,7 @@ struct xen_sysctl {
 #define XEN_SYSCTL_livepatch_op                  27
 #define XEN_SYSCTL_set_parameter                 28
 #define XEN_SYSCTL_get_cpu_policy                29
+#define XEN_SYSCTL_spec_ctrl                    188
     uint32_t interface_version; /* XEN_SYSCTL_INTERFACE_VERSION */
     union {
         struct xen_sysctl_readconsole       readconsole;
@@ -1094,6 +1100,7 @@ struct xen_sysctl {
 #if defined(__i386__) || defined(__x86_64__)
         struct xen_sysctl_cpu_policy        cpu_policy;
 #endif
+        struct xen_sysctl_spec_ctrl         spec_ctrl;
         uint8_t                             pad[128];
     } u;
 };
