986cdf35fbc540ca1b764497620b83c1e5b8e6f7
Sergey Dyasli <sergey.dyasli@citrix.com>
2019-07-23 16:49:29 +0100

    xsm: hide detailed Xen version from unprivileged guests
    These subops leak information into guests which they have no buisness
    knowning.  Return empty strings instead.
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/include/xsm/dummy.h
+++ xen/include/xsm/dummy.h
@@ -796,14 +796,22 @@ static XSM_INLINE int xsm_xen_version(XSM_DEFAULT_ARG uint32_t op)
     case XENVER_get_features:
         /* These sub-ops ignore the permission checks and return data. */
         return 0;
-    case XENVER_extraversion:
-    case XENVER_compile_info:
-    case XENVER_capabilities:
-    case XENVER_changeset:
+
     case XENVER_pagesize:
     case XENVER_guest_handle:
         /* These MUST always be accessible to any guest by default. */
         return xsm_default_action(XSM_HOOK, current->domain, NULL);
+
+    case XENVER_extraversion:
+    case XENVER_compile_info:
+    case XENVER_capabilities:
+    case XENVER_changeset:
+    case XENVER_commandline:
+        if ( IS_ENABLED(CONFIG_DEBUG) )
+            /* Expose information to guests only in debug builds. */
+            return xsm_default_action(XSM_HOOK, current->domain, NULL);
+
+        /* fallthrough */
     default:
         return xsm_default_action(XSM_PRIV, current->domain, NULL);
     }
