d802a00f11362b2a37ff1f6f8ed31e6e15073a7b
Andrew Cooper <andrew.cooper3@citrix.com>
2019-10-31 20:27:11 +0000

    x86/msr: Expose cpu_has_tsx_ctrl via MSR_ARCH_CAPS
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/lib/x86/msr.c
+++ xen/lib/x86/msr.c
@@ -40,6 +40,18 @@ int x86_msr_copy_to_buffer(const struct msr_policy *p,
 
     COPY_MSR(MSR_INTEL_PLATFORM_INFO, p->platform_info.raw);
 
+#ifdef __XEN__
+    /*
+     * When requesting the Host MSR policy, feed back cpu_has_tsx_ctrl in its
+     * architectural position in MSR_ARCH_CAPS, so the toolstack can evaluate
+     * whether a VM migrating in having seen TSX is safe to run.  Bodge
+     * cpu_has_rtm_always_abort as TSX_CTRL too for the June 2021 changes.
+     */
+    if ( p == &host_msr_policy && (cpu_has_tsx_ctrl == 1 ||
+                                   cpu_has_rtm_always_abort) )
+        COPY_MSR(MSR_ARCH_CAPABILITIES, ARCH_CAPS_TSX_CTRL);
+#endif
+
 #undef COPY_MSR
 
     *nr_entries_p = curr_entry;
