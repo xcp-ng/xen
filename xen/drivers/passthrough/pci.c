8f6393ce88811f98b78b61574f2bd5d36d13821f
Igor Druzhinin <igor.druzhinin@citrix.com>
2019-12-05 13:31:03 +0100

    passthrough: drop break statement following c/s cd7dedad820
    The locking responsibilities have changed and a premature break in
    this section now causes the following assertion:
    
    Assertion '!preempt_count()' failed at preempt.c:36
    
    Reported-by: Sander Eikelenboom <linux@eikelenboom.it>
    Suggested-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Igor Druzhinin <igor.druzhinin@citrix.com>
    Reviewed-by: Paul Durrant <pdurrant@amazon.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/drivers/passthrough/pci.c
+++ xen/drivers/passthrough/pci.c
@@ -1730,7 +1730,6 @@ int iommu_do_pci_domctl(
                        seg, bus, PCI_SLOT(devfn), PCI_FUNC(devfn));
                 ret = -EINVAL;
             }
-            break;
         }
         else if ( !ret )
             ret = assign_device(d, seg, bus, devfn, flags);
