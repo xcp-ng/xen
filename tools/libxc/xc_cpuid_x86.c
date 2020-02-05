d60629d257d58bf333bdb0dd40ef8d5d9cfcdc7c
Andrew Cooper <andrew.cooper3@citrix.com>
2020-02-05 13:30:18 +0000

    tools/libx[cl]: Don't use HVM_PARAM_PAE_ENABLED as a function parameter
    HVM_PARAM_PAE_ENABLED is set and consumed by the toolstack only.  It is in
    practice a complicated and non-standard way of passing a boolean parameter
    into xc_cpuid_apply_policy().
    
    This is silly.  Pass PAE as a regular parameter instead.
    
    In libxl__cpuid_legacy(), leave a rather better explaination of why only HVM
    guests have a choice in PAE setting.
    
    No change in how a guest is constructed.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/libxc/xc_cpuid_x86.c
+++ tools/libxc/xc_cpuid_x86.c
@@ -455,7 +455,8 @@ int xc_cpuid_set(
 }
 
 int xc_cpuid_apply_policy(xc_interface *xch, uint32_t domid,
-                          const uint32_t *featureset, unsigned int nr_features)
+                          const uint32_t *featureset, unsigned int nr_features,
+                          bool pae)
 {
     int rc;
     xc_dominfo_t di;
@@ -634,15 +635,7 @@ int xc_cpuid_apply_policy(xc_interface *xch, uint32_t domid,
             break;
         }
 
-        /*
-         * HVM_PARAM_PAE_ENABLED is a parameter to this function, stashed in
-         * Xen.  Nothing else has ever taken notice of the value.
-         */
-        rc = xc_hvm_param_get(xch, domid, HVM_PARAM_PAE_ENABLED, &val);
-        if ( rc )
-            goto out;
-
-        p->basic.pae = val;
+        p->basic.pae = pae;
 
         /*
          * These settings are necessary to cause earlier HVM_PARAM_NESTEDHVM /
