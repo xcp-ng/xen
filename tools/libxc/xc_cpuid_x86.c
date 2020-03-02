2f1085e47dcb3cf40dca47ff87824b24dc88cc66
Andrew Cooper <andrew.cooper3@citrix.com>
2020-03-02 14:36:03 +0000

    tools/libxc: Reduce feature handling complexity in xc_cpuid_apply_policy()
    xc_cpuid_apply_policy() is gaining extra parameters to untangle CPUID
    complexity in Xen.  While an improvement in general, it does have the
    unfortunate side effect of duplicating some settings across multiple
    parameters.
    
    Rearrange the logic to only consider 'pae' if no explicit featureset is
    provided.  This reduces the complexity for callers who have already provided a
    pae setting in the featureset.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Paul Durrant <pdurrant@amzn.com>
    Acked-by: Wei Liu <wl@xen.org>

--- tools/libxc/xc_cpuid_x86.c
+++ tools/libxc/xc_cpuid_x86.c
@@ -550,6 +550,11 @@ int xc_cpuid_apply_policy(xc_interface *xch, uint32_t domid,
 
         cpuid_featureset_to_policy(feat, p);
     }
+    else
+    {
+        if ( di.hvm )
+            p->basic.pae = pae;
+    }
 
     if ( !di.hvm )
     {
@@ -634,8 +639,6 @@ int xc_cpuid_apply_policy(xc_interface *xch, uint32_t domid,
             }
             break;
         }
-
-        p->basic.pae = pae;
 
         /*
          * These settings are necessary to cause earlier HVM_PARAM_NESTEDHVM /
