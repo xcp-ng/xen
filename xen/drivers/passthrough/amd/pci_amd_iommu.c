10450891e171ceb81e74675fa43fb0d1f96ecd1f
Jan Beulich <jbeulich@suse.com>
2020-03-10 15:25:58 +0100

    AMD/IOMMU: without XT, x2APIC needs to be forced into physical mode
    The wider cluster mode APIC IDs aren't generally representable. Convert
    the iommu_intremap variable into a tristate, allowing the AMD IOMMU
    driver to signal this special restriction to the apic_x2apic_probe().
    (Note: assignments to the variable get adjusted, while existing
    consumers - all assuming a boolean property - are left alone.)
    
    While we are not aware of any hardware/firmware with this as a
    restriction, it is a situation which could be created on fully x2apic-
    capable systems via firmware settings.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- xen/drivers/passthrough/amd/pci_amd_iommu.c
+++ xen/drivers/passthrough/amd/pci_amd_iommu.c
@@ -276,7 +276,7 @@ int __init acpi_ivrs_init(void)
 
     if ( (amd_iommu_detect_acpi() !=0) || (iommu_found() == 0) )
     {
-        iommu_intremap = 0;
+        iommu_intremap = iommu_intremap_off;
         return -ENODEV;
     }
 
