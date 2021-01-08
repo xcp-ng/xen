e09ca2df14eb77929ad9fe865321a45c8de95d03
Edwin Török <edvin.torok@citrix.com>
2021-01-08 11:57:37 +0000

    tools/oxenstored: Only quit on SIGTERM when a reload is possible
    Currently when oxenstored receives SIGTERM it dumps its state and quits.  It
    is possible to then restart it if --restart is given, however that is not
    always safe:
    
    * Domains could have active transactions, and after a restart they would
      either reuse transaction IDs of already open transactions, or get an error
      back that the transaction doesn't exist
    
    * There could be pending data to send to a VM still in oxenstored's
      queue which would be lost
    
    * There could be pending input to be processed from a VM in oxenstored's
      queue which would be lost
    
    Prevent shutting down oxenstored via SIGTERM in the above situations.  Also
    ignore domains marked as bad because oxenstored would never talk to them
    again.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>
    Reviewed-by: Pau Ruiz Safont <pau.safont@citrix.com>
    Reviewed-by: Christian Lindig <christian.lindig@citrix.com>

--- tools/ocaml/xenstored/xenstored.ml
+++ tools/ocaml/xenstored/xenstored.ml
@@ -20,6 +20,7 @@ open Parse_arg
 open Stdext
 
 let error fmt = Logging.error "xenstored" fmt
+let warn fmt = Logging.warn "xenstored" fmt
 let debug fmt = Logging.debug "xenstored" fmt
 let info fmt = Logging.info "xenstored" fmt
 
@@ -378,7 +379,9 @@ let _ =
 	);
 
 	Sys.set_signal Sys.sighup (Sys.Signal_handle sighup_handler);
-	Sys.set_signal Sys.sigterm (Sys.Signal_handle (fun _ -> quit := true));
+	Sys.set_signal Sys.sigterm (Sys.Signal_handle (fun _ ->
+		info "Received SIGTERM";
+		quit := true));
 	Sys.set_signal Sys.sigusr1 (Sys.Signal_handle (fun _ -> sigusr1_handler store));
 	Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
 
@@ -490,6 +493,12 @@ let _ =
 		);
 		let elapsed = Unix.gettimeofday () -. now in
 		debug "periodic_ops took %F seconds." elapsed;
+		if !quit then (
+			match Connections.prevents_quit cons with
+			| [] -> ()
+			| domains -> List.iter (fun con -> warn "%s prevents live update"
+								(Connection.get_domstr con)) domains
+		);
 		delay_next_frequent_ops_by elapsed
 	in
 
@@ -541,7 +550,7 @@ let _ =
 		in
 
 	Systemd.sd_notify_ready ();
-	while not !quit
+	while not (!quit && Connections.prevents_quit cons = [])
 	do
 		try
 			main_loop ()
