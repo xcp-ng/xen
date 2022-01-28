da8db82bd81a7d30479fb8ed3fd77f579466f0b8
Andrew Cooper <andrew.cooper3@citrix.com>
2022-01-28 11:57:19 +0000

    x86/spec-ctrl: Record the last write to MSR_SPEC_CTRL
    In some cases, writes to MSR_SPEC_CTRL do not have interesting side effects,
    and we should implement lazy context switching like we do with other MSRs.
    
    In the short term, this will be used by the SVM infrastructure, but I expect
    to extend it to other contexts in due course.
    
    Introduce cpu_info.last_spec_ctrl for the purpose, and cache writes made from
    the boot/resume paths.  The value can't live in regular per-cpu data when it
    is eventually used for PV guests when XPTI might be active.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    (cherry picked from commit 00f2992b6c7a9d4090443c1a85bf83224a87eeb9)

--- xen/arch/x86/spec_ctrl.c
+++ xen/arch/x86/spec_ctrl.c
@@ -1294,6 +1294,9 @@ void __init init_speculation_mitigations(void)
      */
     if ( has_spec_ctrl )
     {
+        struct cpu_info *info = get_cpu_info();
+        unsigned int val;
+
         bsp_delay_spec_ctrl = !cpu_has_hypervisor && default_xen_spec_ctrl;
 
         /*
@@ -1302,15 +1305,16 @@ void __init init_speculation_mitigations(void)
          */
         if ( bsp_delay_spec_ctrl )
         {
-            struct cpu_info *info = get_cpu_info();
-
             info->shadow_spec_ctrl = 0;
             barrier();
             info->spec_ctrl_flags |= SCF_use_shadow;
             barrier();
         }
 
-        wrmsrl(MSR_SPEC_CTRL, bsp_delay_spec_ctrl ? 0 : default_xen_spec_ctrl);
+        val = bsp_delay_spec_ctrl ? 0 : default_xen_spec_ctrl;
+
+        wrmsrl(MSR_SPEC_CTRL, val);
+        info->last_spec_ctrl = val;
     }
 
     if ( boot_cpu_has(X86_FEATURE_SRBDS_CTRL) )
