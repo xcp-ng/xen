3aba880b031651b2a7fade4efbea67f8e6b4115e
Roger Pau Monné <roger.pau@citrix.com>
2022-05-18 11:21:49 +0200

    x86/cpuid: expose MCDT_NO to guests
    Expose the MCDT_NO CPUID flag to guests if available.  The bit signals
    that the CPU does not exhibit MCDT behavior, and as such can be
    exposed without requiring any additional work.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- tools/misc/xen-cpuid.c
+++ tools/misc/xen-cpuid.c
@@ -194,6 +194,8 @@ static const char *const str_7b1[32] =
 static const char *const str_7d2[32] =
 {
     [ 0] = "intel-psfd",
+
+    /* 4 */                 [ 5] = "mcdt-no",
 };
 
 static const struct {
