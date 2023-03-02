da8c776b275b42e297c162a37432101c6a09d2b1
Sergey Dyasli <sergey.dyasli@citrix.com>
2023-03-02 19:54:37 +0000

    Add PV-IOMMU elf note
    Add XS_ELFNOTE_PV_IOMMU which Dom0 kernel will set if it knows how
    to handle PV-IOMMU.  Introduce xs_elf_notes for XenServer-specific
    ELF notes in order not to modify the upstream ABI.
    
    Add IOMMU_FEAT_XS_PV_IOMMU to let pv-iommu layer to know about the
    presence of the new ELF note.
    
    Signed-off-by: Sergey Dyasli <sergey.dyasli@citrix.com>

--- xen/arch/x86/pv/dom0_build.c
+++ xen/arch/x86/pv/dom0_build.c
@@ -1027,7 +1027,11 @@ static int __init dom0_construct(const struct boot_domain *bd)
     BUG_ON(rc != 0);
 
     if ( d->domain_id == hardware_domid )
+    {
+        if ( parms.xs_elf_notes[XS_ELFNOTE_PV_IOMMU].data.num != 0 )
+            iommu_set_feature(d, IOMMU_FEAT_XS_PV_IOMMU);
         iommu_hwdom_init(d);
+    }
 
 #ifdef CONFIG_SHADOW_PAGING
     /* Fill the shadow pool if necessary. */
