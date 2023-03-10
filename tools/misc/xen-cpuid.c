69f9948081b94a3d2d7facc70c09d94a99bb394f
Andrew Cooper <andrew.cooper3@citrix.com>
2023-03-10 18:14:13 +0000

    tools/xen-cpuid: Support dash as a bitmap separator
    Xapi chose to render its featureset bitmaps with dashes rather than colons as
    a separator.  Have xen-cpuid support both forms, for convenience.
    
    Fix a trivial style issue in the vicinity while here.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- tools/misc/xen-cpuid.c
+++ tools/misc/xen-cpuid.c
@@ -580,9 +580,10 @@ int main(int argc, char **argv)
                 if ( i == nr_features )
                     break;
 
-                if ( *ptr == ':' )
+                if ( *ptr == ':' || *ptr == '-' )
                 {
-                    ptr++; continue;
+                    ptr++;
+                    continue;
                 }
                 break;
             }
