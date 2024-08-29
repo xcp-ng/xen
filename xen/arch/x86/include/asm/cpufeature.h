b91c4d625380a8bae0ea9cdb81e43012bcb006ab
Andrew Cooper <andrew.cooper3@citrix.com>
2024-08-29 17:36:11 +0100

    x86/spec-ctrl: Mitigate Transitive Scheduler Attacks
    TSA affects AMD Fam19h CPUs (Zen3 and 4 microarchitectures).
    
    Three new CPUID bits have been defined.  Two (TSA_SQ_NO and TSA_L1_NO)
    indicate that the system is unaffected, and must be synthesised by Xen on
    unaffected parts to date.
    
    A third new bit indicates that VERW now has a flushing side effect.  Xen must
    synthesise this bit on affected systems based on microcode version.  As with
    other VERW-based flusing features, VERW_CLEAR needs OR-ing across a resource
    pool, and guests which have seen it can safely migrate in.
    
    This is part of XSA-471 / CVE-2024-36350 / CVE-2024-36357.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/include/asm/cpufeature.h
+++ xen/arch/x86/include/asm/cpufeature.h
@@ -153,6 +153,7 @@
 
 /* CPUID level 0x80000021.eax */
 #define cpu_has_lfence_dispatch boot_cpu_has(X86_FEATURE_LFENCE_DISPATCH)
+#define cpu_has_verw_clear      boot_cpu_has(X86_FEATURE_VERW_CLEAR)
 #define cpu_has_nscb            boot_cpu_has(X86_FEATURE_NSCB)
 
 /* MSR_ARCH_CAPS */
@@ -173,6 +174,10 @@
 #define cpu_has_rfds_clear      boot_cpu_has(X86_FEATURE_RFDS_CLEAR)
 #define cpu_has_pb_opt_ctrl     boot_cpu_has(X86_FEATURE_PB_OPT_CTRL)
 #define cpu_has_its_no          boot_cpu_has(X86_FEATURE_ITS_NO)
+
+/* CPUID level 0x80000021.ecx */
+#define cpu_has_tsa_sq_no       boot_cpu_has(X86_FEATURE_TSA_SQ_NO)
+#define cpu_has_tsa_l1_no       boot_cpu_has(X86_FEATURE_TSA_L1_NO)
 
 /* Synthesized. */
 #define cpu_has_arch_perfmon    boot_cpu_has(X86_FEATURE_ARCH_PERFMON)
