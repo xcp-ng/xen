c220898a758b2079434b0ff2eac21fd48c1b0443
Roger Pau Monne <roger.pau@citrix.com>
2025-06-09 16:43:44 +0200

    pdx: provide a common set of init functions
    The current setup (pdx_init_mask() and pdx_region_mask()) and init
    (pfn_pdx_hole_setup()) PDX compression functions are tailored to the
    existing PDX compression algorithm.
    
    In preparation for introducing a new compression algorithm convert the
    setup and init functions to more generic interfaces that aren't tied to the
    compression in-use.  To accomplish this introduce a function that registers
    all the PFN RAM ranges, plus an init function.
    
    This has the downside of requiring a static array to store such ranges
    ahead of being processed by the setup function, however it's the only way
    to pass all the possible information to the different compression setup
    functions without using per-compression specific setup functions.
    Per-arch compression setup also need to be adjusted to use the new
    interface.  There's a slight ordering adjustment, in that after PDX
    compression setup the caller will check whether all the RAM regions are
    properly covered by the newly setup compression, otherwise compression is
    disabled by resetting to the initial values.
    
    No functional change intended in the resulting PDX compression values.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Julien Grall <jgrall@amazon.com> # ARM

--- xen/arch/x86/srat.c
+++ xen/arch/x86/srat.c
@@ -492,8 +492,6 @@ static int __init nodes_cover_memory(void)
 
 void __init acpi_numa_arch_fixup(void) {}
 
-static uint64_t __initdata srat_region_mask;
-
 static int __init cf_check srat_parse_region(
     struct acpi_subtable_header *header, const unsigned long end)
 {
@@ -513,34 +511,42 @@ static int __init cf_check srat_parse_region(
 		printk(KERN_INFO "SRAT: %013"PRIx64"-%013"PRIx64"\n",
 		       ma->base_address, ma->base_address + ma->length - 1);
 
-	srat_region_mask |= ma->base_address |
-			    pdx_region_mask(ma->base_address, ma->length);
+	pfn_pdx_add_region(ma->base_address, ma->length);
 
 	return 0;
 }
 
 void __init srat_parse_regions(paddr_t addr)
 {
-	u64 mask;
 	unsigned int i;
 
 	if (acpi_disabled || acpi_numa < 0 ||
 	    acpi_table_parse(ACPI_SIG_SRAT, acpi_parse_srat))
 		return;
 
-	srat_region_mask = pdx_init_mask(addr);
 	acpi_table_parse_srat(ACPI_SRAT_TYPE_MEMORY_AFFINITY,
 			      srat_parse_region, 0);
 
-	for (mask = srat_region_mask, i = 0; mask && i < e820.nr_map; i++) {
+	pfn_pdx_compression_setup(addr);
+
+	/* Ensure all RAM ranges in the e820 are covered. */
+	for (i = 0; i < e820.nr_map; i++) {
 		if (e820.map[i].type != E820_RAM)
 			continue;
 
-		if (~mask & pdx_region_mask(e820.map[i].addr, e820.map[i].size))
-			mask = 0;
+		if (!pdx_is_region_compressible(
+		          e820.map[i].addr,
+		          PFN_UP(e820.map[i].addr + e820.map[i].size) -
+		          PFN_DOWN(e820.map[i].addr))) {
+			pfn_pdx_compression_reset();
+			printk(XENLOG_WARNING
+			       "PFN compression disabled, RAM region [%#" PRIx64
+			       ", %#" PRIx64 "] not covered\n",
+			       e820.map[i].addr,
+			       e820.map[i].addr + e820.map[i].size - 1);
+			return;
+		}
 	}
-
-	pfn_pdx_hole_setup(mask >> PAGE_SHIFT);
 }
 
 /* Use the information discovered above to actually set up the nodes. */
