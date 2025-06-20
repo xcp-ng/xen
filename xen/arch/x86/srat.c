1bf3a3376b27326b762ec4e6ee27ac73c5a432c9
Roger Pau Monne <roger.pau@citrix.com>
2025-06-20 09:50:51 +0200

    pdx: allow per-arch optimization of PDX conversion helpers
    There are four performance critical PDX conversion helpers that do the PFN
    to/from PDX and the physical addresses to/from directmap offsets
    translations.
    
    In the absence of an active PDX compression, those functions would still do
    the calculations needed, just to return the same input value as no
    translation is in place and hence PFN and PDX spaces are identity mapped.
    
    To reduce the overhead of having to do the pointless calculations allow
    architectures to implement the translation helpers in a per-arch header.
    Rename the existing conversion functions to add a trailing _xlate suffix,
    so that the per-arch headers can define the non suffixed versions.
    
    Currently only x86 implements meaningful custom handlers to short circuit
    the translation when not active, using asm goto.  Other architectures use
    generic macros that map the non-xlate to the xlate variants to keep the
    previous behavior.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/srat.c
+++ xen/arch/x86/srat.c
@@ -527,7 +527,8 @@ void __init srat_parse_regions(paddr_t addr)
 	acpi_table_parse_srat(ACPI_SRAT_TYPE_MEMORY_AFFINITY,
 			      srat_parse_region, 0);
 
-	pfn_pdx_compression_setup(addr);
+	if (!pfn_pdx_compression_setup(addr))
+		return;
 
 	/* Ensure all RAM ranges in the e820 are covered. */
 	for (i = 0; i < e820.nr_map; i++) {
@@ -547,6 +548,9 @@ void __init srat_parse_regions(paddr_t addr)
 			return;
 		}
 	}
+
+	/* If we got this far compression is working as expected. */
+	setup_force_cpu_cap(X86_FEATURE_PDX_COMPRESSION);
 }
 
 /* Use the information discovered above to actually set up the nodes. */
