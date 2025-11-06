245d107c622c3d4c246c9336c26ead0179e58390
Th0rOnDoR <thomas.courrege@vates.tech>
2025-11-06 11:24:28 +0100

    fix(sev): typo in domctl.c

--- xen/arch/x86/domctl.c
+++ xen/arch/x86/domctl.c
@@ -154,11 +154,9 @@ void arch_get_domain_info(const struct domain *d,
     info->arch_config.emulation_flags = d->arch.emulation_flags;
     info->gpaddr_bits = hap_paddr_bits;
     #ifdef CONFIG_COCO_AMD_SEV
-    if ( is_sev_domain(d))
+    if ( is_sev_domain(d) )
     {
-        // info->arch_config.coco.sev.flags = XEN_X86_SEV_POLICY_VALID;
         info->arch_config.coco.sev.flags = XEN_X86_SEV_POLICY_VALID;
-        // info->arch_config.coco.sev.policy = d->arch.hvm.svm.sev.asp_policy.raw;
         info->arch_config.coco.sev.policy = d->arch.hvm.svm.sev.asp_policy.raw;
     }
     #endif
