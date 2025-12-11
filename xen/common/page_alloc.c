25e2b57124fb2fc2e714b7d2b6798fab08c27e3a
Andrew Cooper <andrew.cooper3@citrix.com>
2025-12-11 18:22:53 +0000

    domain_adjust_tot_pages() node param
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/common/page_alloc.c
+++ xen/common/page_alloc.c
@@ -513,7 +513,7 @@ static unsigned long avail_heap_pages(
     return free_pages;
 }
 
-unsigned long domain_adjust_tot_pages(struct domain *d, long pages)
+unsigned long domain_adjust_tot_pages(struct domain *d, nodeid_t node, long pages)
 {
     ASSERT(rspin_is_locked(&d->page_alloc_lock));
     d->tot_pages += pages;
@@ -2716,7 +2716,7 @@ int assign_pages(
             goto out;
         }
 
-        if ( unlikely(domain_adjust_tot_pages(d, nr) == nr) )
+        if ( unlikely(domain_adjust_tot_pages(d, page_to_nid(pg), nr) == nr) )
             get_knownalive_domain(d);
     }
 
@@ -2858,7 +2858,7 @@ void free_domheap_pages(struct page_info *pg, unsigned int order)
                 }
             }
 
-            drop_dom_ref = !domain_adjust_tot_pages(d, -(1 << order));
+            drop_dom_ref = !domain_adjust_tot_pages(d, page_to_nid(pg), -(1 << order));
 
             rspin_unlock(&d->page_alloc_lock);
 
@@ -3067,7 +3067,7 @@ void free_domstatic_page(struct page_info *page)
 
     arch_free_heap_page(d, page);
 
-    drop_dom_ref = !domain_adjust_tot_pages(d, -1);
+    drop_dom_ref = !domain_adjust_tot_pages(d, page_to_nid(page), -1);
 
     unprepare_staticmem_pages(page, 1, scrub_debug);
 
