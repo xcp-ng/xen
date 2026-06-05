87eaf8cf0a6a4d492d29b831ba5f21303e194806
Jan Beulich <jbeulich@suse.com>
2026-06-05 15:40:19 +0200

    domctl/XSM: drop {,de}assign_{,dt}device hooks
    Integrate the checking with xsm_domctl(). As a positive side effect,
    permissions are then checked at the same early point with and without
    Flask. As the DT device path needs fetching earlier (but must not be
    double fetched), cache it in a private field of the public interface
    struct.
    
    This is part of XSA-492.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/drivers/passthrough/pci.c
+++ xen/drivers/passthrough/pci.c
@@ -1699,10 +1699,6 @@ int iommu_do_pci_domctl(
 
         machine_sbdf = domctl->u.assign_device.u.pci.machine_sbdf;
 
-        ret = xsm_assign_device(XSM_HOOK, d, machine_sbdf);
-        if ( ret )
-            break;
-
         seg = machine_sbdf >> 16;
         bus = PCI_BUS(machine_sbdf);
         devfn = PCI_DEVFN(machine_sbdf);
@@ -1743,10 +1739,6 @@ int iommu_do_pci_domctl(
             break;
 
         machine_sbdf = domctl->u.assign_device.u.pci.machine_sbdf;
-
-        ret = xsm_deassign_device(XSM_HOOK, d, machine_sbdf);
-        if ( ret )
-            break;
 
         seg = machine_sbdf >> 16;
         bus = PCI_BUS(machine_sbdf);
