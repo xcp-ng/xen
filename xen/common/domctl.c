564be57f8feb9b77ae76c91259955abdf0ff83fa
Jan Beulich <jbeulich@suse.com>
2026-06-16 14:17:30 +0200

    domctl: error code adjustment for unpriv callers
    Unprivileged callers better wouldn't be in the position of figuring out
    domain existence from error codes. Adjust the respective path sitting
    ahead of XSM checks to produce -EPERM in such cases, just like the
    subsequent XSM check would yield.
    
    Suggested-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Roger Pau Monné <roger.pau@citrix.com>
    Release-Acked-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/common/domctl.c
+++ xen/common/domctl.c
@@ -366,7 +366,8 @@ long do_domctl(XEN_GUEST_HANDLE_PARAM(xen_domctl_t) u_domctl)
     default:
         d = rcu_lock_domain_by_id(op->domain);
         if ( !d )
-            return -ESRCH;
+            return is_control_domain(current->domain) ? -ESRCH : -EPERM;
+        break;
     }
 
     /* Handle sub-ops not requiring the domctl lock. */
